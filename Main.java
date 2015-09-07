6849533dd69cf100b7039f1f7442fab08b3c935d
mgg895 <mgg895-bt@hotmail.com>
2015-09-07 16:01:36 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
 # Hoja7
+
+public class AlgoritmoHuffman {
+
+    public static void main(String[] args) {
+        BinaryTree arbol1;
+        BinaryTree arbol2;
+        BinaryTree arbol3;
+        arbol1 = new BinaryTree ("sho", 4, null);
+        arbol1.crearDerecha ("cerote", 8, arbol1);
+        arbol1.crearIzquierda ("puta", 21, arbol1);
+        
+    }
+    
+}
