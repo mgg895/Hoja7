5b29d97b293d8115b784e0afbd53e4da3c98ad69
danielsmithu <smi14089@uvg.edu.gt>
2015-09-08 16:16:51 -0600

    Update AlgoritmoHuffman.java

--- AlgoritmoHuffman.java
+++ AlgoritmoHuffman.java
@@ -7,6 +7,7 @@ public class AlgoritmoHuffman {
     public static void main(String[] args) {
        Scanner teclado=new Scanner(System.in);
         String mensaje;
+        HashSet<Character> HashCadena = new HashSet<>();
         char [] ArrayCadena;
                 
         System.out.print("Ingrese el mensaje:");
@@ -14,23 +15,34 @@ public class AlgoritmoHuffman {
         ArrayCadena=mensaje.toCharArray();
         
         for (int i=0; i < ArrayCadena.length; i++){
-			if (ArrayCadena[i] != ' '){
-				HashCadena.add(ArrayCadena[i]);
-			}
+		if (ArrayCadena[i] != ' '){
+			HashCadena.add(ArrayCadena[i]);
 		}
+	}
 		
-		ArrayList<Integer> ArrayFrecuencia = new ArrayList<Integer>();
+	ArrayList<Integer> ArrayFrecuencia = new ArrayList<Integer>();
 
         for(char a : HashCadena){
-				int e = 0;
-				for(int j=0;j< ArrayCadena.length;j++){
-					if (a == ArrayCadena[j]){
-						e++;
-					}
-				}
-				ArrayFrecuencia.add(e);
+		int e = 0;
+		for(int j=0;j< ArrayCadena.length;j++){
+			if (a == ArrayCadena[j]){
+				e++;
+			}
 		}
-          
+		ArrayFrecuencia.add(e);
+	}
+         
+        //Une la frecuencia con el character
+	String[] ArrayElementos = new String[HashCadena.size()];
+	int val = 0;
+	for(char j : HashCadena){
+		ArrayElementos[val] = (Integer.toString(ArrayFrecuencia.get(val)) + j);
+		val++;
+	}
+		
+	//Ordena el Array de menor a mayor
+	Arrays.sort(ArrayElementos);
+		
         System.out.println(ArrayFrecuencia);
 
         BinaryTree arbol1;
