291aae10849bafa055f8f609729c76ee00e196ca
mgg895 <mgg895-bt@hotmail.com>
2015-09-08 23:11:23 -0600

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,24 +1,52 @@
-public class BinaryTree {
+public class BinaryTree implements Comparable<BinaryTree>{
     
-    protected String valor;
+    protected char valor;
     protected int frecuencia;
     protected BinaryTree padre;
     
-    public BinaryTree(String valor, int frecuencia, BinaryTree padre){
+    public BinaryTree(char valor, int frecuencia, BinaryTree padre){
         this.valor = valor;
         this.frecuencia = frecuencia;
         this.padre = padre;
     }
     
-    public BinaryTree crearDerecha(String valorDerecha, int frecuenciaDerecha, BinaryTree self){
+    public char getValor(){
+        return valor;
+    }
+    
+    public int getFrecuencia(){
+        return frecuencia;
+    }
+    
+    @Override
+    public int compareTo(BinaryTree other){
+        int comparacion;
+        if (other.getFrecuencia() >= frecuencia){
+            comparacion = 1;
+        } else {
+            comparacion = 0;
+        }
+        return comparacion;
+    }
+    
+    public BinaryTree crearDerecha(char valorDerecha, int frecuenciaDerecha, BinaryTree self){
         BinaryTree hijoDerecha;
         hijoDerecha = new BinaryTree(valorDerecha, frecuenciaDerecha, self);
         return hijoDerecha;
     }
     
-    public BinaryTree crearIzquierda(String valorIzquierda, int frecuenciaIzquierda, BinaryTree self){
+    public BinaryTree crearIzquierda(char valorIzquierda, int frecuenciaIzquierda, BinaryTree self){
         BinaryTree hijoIzquierda;
         hijoIzquierda = new BinaryTree(valorIzquierda, frecuenciaIzquierda, self);
         return hijoIzquierda;
+    }
+    
+    @Override
+    public String toString(){
+        String a = "";
+        a = a+valor;
+        a = a+" ";
+        a = a+frecuencia;
+        return a;
     }
 }
