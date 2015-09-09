405d66296c2c52e55f03e5c55b381b396405dcba
mgg895 <mgg895-bt@hotmail.com>
2015-09-08 23:54:27 -0600

    Update AlgoritmoHuffman.java

--- AlgoritmoHuffman.java
+++ AlgoritmoHuffman.java
@@ -1,65 +1,48 @@
-import java.util.Scanner;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AlgoritmoHuffman {
 
     public static void main(String[] args) {
-       Scanner teclado=new Scanner(System.in);
-       String mensaje;
-	   HashSet<Character> HashCadena = new HashSet<>();
+        
+        Scanner teclado=new Scanner(System.in);
+        String mensaje;
         char [] ArrayCadena;
+        String [] codigos;
                 
         System.out.print("Ingrese el mensaje:");
         mensaje= teclado.nextLine();
         ArrayCadena=mensaje.toCharArray();
         
-		for (int i=0; i < ArrayCadena.length; i++){
-			if (ArrayCadena[i] != ' '){
-				HashCadena.add(ArrayCadena[i]);
-			}
-		}
-		
-		System.out.println(HashCadena);
-		
-		int[] ArrayFrecuencia = new int[HashCadena.size()];
-		//Se obtiene la frecuencia de cada letra en el arreglo
-		int val = 0;
-		for(char a : HashCadena){
-				int e = 0;
-				for(int j=0;j< ArrayCadena.length;j++){
-					if (a == ArrayCadena[j]){
-						e++;
-					}
-				}
-				ArrayFrecuencia[val] = e;
-				val++;
-		}
-		
-		
-		
-		//Une la frecuencia con el character
-		String[] ArrayElementos = new String[HashCadena.size()];
-		val = 0;
-		for(char j : HashCadena){
-			ArrayElementos[val] = (Integer.toString(ArrayFrecuencia[val]) + j);
-			val++;
-		}
-		
-		//Ordena el Array de menor a mayor
-		
-		Arrays.sort(ArrayElementos);
-		Arrays.sort(ArrayFrecuencia);
+        int [] ArrayFrecuencia ;
+        BinaryTree [] ArrayArboles;
+        
+        ArrayFrecuencia = new int[ArrayCadena.length];
+        ArrayArboles = new BinaryTree[ArrayCadena.length];
+        codigos = new String[ArrayCadena.length];
 
-        System.out.println(Arrays.toString(ArrayFrecuencia));
-		System.out.println(Arrays.toString(ArrayElementos));
-
-       BinaryTree<String> arbol = new BinaryTree<String> ();
-	   
-	   for (int i=0;i<HashCadena.size()+1;i++){
-		   BinaryTree<String> nuevoArbol = arbol.crearDerecha (ArrayElementos[i], ArrayFrecuencia[i], arbol);
-		   //arbol3=arbol1.crearIzquierda ("p", 21, arbol1);
-		   System.out.println(nuevoArbol);
-	   }
+          for(int i=0;i< ArrayCadena.length;i++){
+              char c = ArrayCadena[i];
+              int e = 1;
+              for(int j=i+1;j< ArrayCadena.length;j++){
+                  if (ArrayCadena[j]==c){
+                      e++;
+                      ArrayCadena[j]=' ';
+                  }
+              }
+              ArrayArboles[i]=new BinaryTree(c,e,null);
+              ArrayFrecuencia[i]=e;
+          }
+        
+        Arrays.sort(ArrayArboles);
+        
+        int codigo =0;
+        for(int i=ArrayCadena.length-1;i>0;i--){
+            codigos[i]= Integer.toBinaryString(codigo++);
+        }
+        
+        System.out.println(Arrays.toString(ArrayArboles));
+        System.out.println(Arrays.toString(codigos));
     }
 }
+
